c0898205e16860d9cb90e8ea83d1f2847d0d99de
Sergei Gagarin <66745577+Lavmee@users.noreply.github.com>
2023-06-27 10:52:28 +0300

    Use internal LocalWindow composition local on Desktop (#13)

--- lib/src/jvmMain/kotlin/androidx/compose/material3/windowsizeclass/WindowSizeClass.desktop.kt
+++ lib/src/jvmMain/kotlin/androidx/compose/material3/windowsizeclass/WindowSizeClass.desktop.kt
@@ -1,19 +1,19 @@
 // Copyright 2023, Christopher Banes and the project contributors
 // SPDX-License-Identifier: Apache-2.0
 
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+
 package androidx.compose.material3.windowsizeclass
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
+import androidx.compose.ui.window.LocalWindow
 import java.awt.Component
 import java.awt.Window
 import java.awt.event.ComponentAdapter
@@ -22,19 +22,8 @@ import java.awt.event.ComponentEvent
 @ExperimentalMaterial3WindowSizeClassApi
 @Composable
 actual fun calculateWindowSizeClass(): WindowSizeClass {
-    var window: Window? by remember { mutableStateOf(null) }
-    var size by remember { mutableStateOf(DpSize.Zero) }
-
-    LaunchedEffect(Unit) {
-        // Yes, this is gross. The LocalWindow composition local is internal, so we need a
-        // 'static' way to retrieve the current window. The only way I could think of was
-        // to poll Window.getWindows(). This might not work in all situations.
-        while (isActive) {
-            window = Window.getWindows().firstOrNull { it.isActive }
-            // Delay for the next poll. If we haven't got a window yet, poll quicker.
-            delay(if (window == null) 50 else 1000)
-        }
-    }
+    val window: Window? = LocalWindow.current
+    var size by remember { mutableStateOf(window?.getDpSize() ?: DpSize.Zero) }
 
     // Add a listener and listen for componentResized events
     DisposableEffect(window) {
