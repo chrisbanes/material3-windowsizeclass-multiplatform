86ce6e89a5cfd4429531abf7f45dae45187d333a
Chris Banes <chris@banes.me>
2023-08-14 16:56:16 +0100

    Use public APIs in JS sample
    The current sample is using a bunch of internal APIs.
    The sample works using the public APIs which are available
    so we should just use those.

--- sample/shared/src/jsMain/kotlin/dev/chrisbanes/material3/windowsizeclass/sample/Sample.js.kt
+++
@@ -1,6 +0,0 @@
-package dev.chrisbanes.material3.windowsizeclass.sample
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun MainView() = Sample()
--- sample/web-app/src/jsMain/kotlin/dev/chrisbanes/material3/windowsizeclass/sample/Main.kt
+++ sample/web-app/src/jsMain/kotlin/dev/chrisbanes/material3/windowsizeclass/sample/Main.kt
@@ -5,7 +5,7 @@ import org.jetbrains.skiko.wasm.onWasmReady
 fun main() {
     onWasmReady {
         BrowserViewportWindow("Sample") {
-            MainView()
+            Sample()
         }
     }
 }
